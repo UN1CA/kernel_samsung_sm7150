679dbe3fa46024a0b38e648480099142ba2525b4
Flopster101 <nahuelgomez329@gmail.com>
2025-11-07 15:52:37 -0300

    drivers: qca-wifi-host-cmn: Disable IRQ affinity management when SBalance is enabled
    Let SBalance handle IRQ affinities when it's enabled.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/hif/src/pcie/if_pci.c
+++ drivers/staging/qca-wifi-host-cmn/hif/src/pcie/if_pci.c
@@ -3639,6 +3639,9 @@ const char *hif_pci_get_irq_name(int irq_no)
 void hif_pci_irq_set_affinity_hint(
 	struct hif_exec_context *hif_ext_group)
 {
+	if (IS_ENABLED(CONFIG_IRQ_SBALANCE))
+		return;
+	
 	int i, ret;
 	unsigned int cpus;
 	bool mask_set = false;
@@ -3742,6 +3745,9 @@ void hif_pci_ce_irq_set_affinity_hint(
 
 void hif_pci_config_irq_affinity(struct hif_softc *scn)
 {
+	if (IS_ENABLED(CONFIG_IRQ_SBALANCE))
+		return;
+	
 	int i;
 	struct HIF_CE_state *hif_state = HIF_GET_CE_STATE(scn);
 	struct hif_exec_context *hif_ext_group;
